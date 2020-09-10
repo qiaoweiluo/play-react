8df58080c361ca89ffbf9fa37594124e2513b105
80004795 <80004795@sfmail.sf-express.com>
2020-09-10 11:39:18 +0800

    学习useMemo&&useCallback

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from "react";
+// , PureComponent, memo, useState, useMemo, useCallback
+// import "./App.css";
 
-function App() {
+function Count(props) {
+  return <div>子组件count值：{props.count}</div>;
+}
+
+function App(props) {
+  const [count, setCount] = useState(0);
+
+  //第一个参数是要执行的函数
+  //第二个参数是执行函数依赖的变量组成的数据
+  //这里只有count发生变化double才会重新计算
+  const double = useMemo(() => {
+    return count * 2;
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>父组件count值：{count}</p>
+      <Count count={count} />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Add
+      </button>
+      <div>double值：{double}</div>
     </div>
   );
 }
